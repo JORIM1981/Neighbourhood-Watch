5da4c79eb836dea4f6a29ac0f406fd4da694590e
JORIM1981 <okongo.midumbi.opondo@gmail.com>
2020-07-19 12:43:52 +0300

    add registration of models in admin.py

--- neighbour/admin.py
+++ neighbour/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile, Neighbourhood , Post, Business
 
 # Register your models here.
+admin.site.register(Neighbourhood)
+admin.site.register(Post)
+admin.site.register(Business)
+admin.site.register(Profile)
